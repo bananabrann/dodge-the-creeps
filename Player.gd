c4c51ce1e1bfb3756901259f07d92a8d5dea59a1
bananabrann <plbrannan95@gmail.com>
2021-04-10 16:47:33 -0400

    Complete game

--- Player.gd
+++ Player.gd
@@ -5,12 +5,11 @@ signal hit
 export var speed = 400
 var screen_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	hide()
 	screen_size = get_viewport_rect().size
+	hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
@@ -26,7 +25,7 @@ func _process(delta):
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-	
+		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
@@ -34,17 +33,20 @@ func _process(delta):
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
+
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-
-func _on_Player_body_entered(body):
-	hide()
-	emit_signal("hit")
-	$CollisionShape2D.set_deferred("disabled", true)
